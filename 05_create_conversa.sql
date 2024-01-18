35ffb286b8edf9dd6fb97edf8d80656714968b39
Gabriel <gabrielteles931@gmail.com>
2024-01-18 11:16:00 -0300

    revertendo  envio de arquivo errado

--- 05_create_conversa.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `mydb`.`conversa` (
-  `idmensagem` INT NOT NULL,
-  `data_hora_inicio` VARCHAR(45) NULL,
-  `remetente_id_usuario` INT NOT NULL,
-  `destinatario_id_usuario1` INT NOT NULL,
-  PRIMARY KEY (`idmensagem`))
-ENGINE = InnoDB;
