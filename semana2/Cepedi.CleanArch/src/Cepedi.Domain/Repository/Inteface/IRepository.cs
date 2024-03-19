006143985798fe62fbbefdc31a077720f194cfda
Lorena Andrade <lorenaandrade@gmail.com>
2024-03-19 11:35:53 -0300

    estrutura clean architectureUpdate README.md

---
+++ semana2/Cepedi.CleanArch/src/Cepedi.Domain/Repository/Inteface/IRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cepedi.Domain.Repository.Inteface
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        
+    }
+}
---
+++ semana2/Cepedi.CleanArch/src/Cepedi.WebApi/Controllers/CursoController.cs
@@ -0,0 +1,22 @@
+﻿using Cepedi.Shareable.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cepedi.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CursoController : ControllerBase
+{
+    private readonly ILogger<CursoController> _logger;
+
+    public CursoController(ILogger<CursoController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("{idCurso}")]
+    public async Task<ActionResult<ObtemCursoResponse>> ConsultarCursoAsync([FromRoute] int idCurso)
+    {
+        return Ok();
+    }
+}
